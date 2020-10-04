78bf10e195bbe0982efd6f6b815aedd718ecd05d
maynero <rowel.maynes@gmail.com>
2020-10-04 16:07:19 +0800

    All pages added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,42 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ButtonsComponent } from './components/pages/buttons/buttons.component';
+import { CardsComponent } from './components/pages/cards/cards.component';
+import { ChartsComponent } from './components/pages/charts/charts.component';
+import { DashboardComponent } from './components/pages/dashboard/dashboard.component';
+import { E404Component } from './components/pages/e404/e404.component';
+import { TablesComponent } from './components/pages/tables/tables.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: DashboardComponent
+  },
+  {
+    path: 'buttons.html',
+    component: ButtonsComponent
+  },
+  {
+    path: 'cards.html',
+    component: CardsComponent
+  },
+  {
+    path: '404.html',
+    component: E404Component
+  },
+  {
+    path: 'tables.html',
+    component: TablesComponent
+  },
+  {
+    path: 'charts.html',
+    component: ChartsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,20 @@ import { TopbarComponent } from './components/topbar/topbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { TopbarSearchComponent } from './components/topbar-search/topbar-search.component';
 import { TopbarNavbarComponent } from './components/topbar-navbar/topbar-navbar.component';
+import { E404Component } from './components/pages/e404/e404.component';
+import { BlankComponent } from './components/pages/blank/blank.component';
+import { ButtonsComponent } from './components/pages/buttons/buttons.component';
+import { CardsComponent } from './components/pages/cards/cards.component';
+import { ChartsComponent } from './components/pages/charts/charts.component';
+import { DashboardComponent } from './components/pages/dashboard/dashboard.component';
+import { ForgotPasswordComponent } from './components/pages/forgot-password/forgot-password.component';
+import { LoginComponent } from './components/pages/login/login.component';
+import { RegisterComponent } from './components/pages/register/register.component';
+import { TablesComponent } from './components/pages/tables/tables.component';
+import { ColorComponent } from './components/pages/utilities/color/color.component';
+import { BorderComponent } from './components/pages/utilities/border/border.component';
+import { AnimationComponent } from './components/pages/utilities/animation/animation.component';
+import { OthersComponent } from './components/pages/utilities/others/others.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +34,21 @@ import { TopbarNavbarComponent } from './components/topbar-navbar/topbar-navbar.
     TopbarComponent,
     FooterComponent,
     TopbarSearchComponent,
-    TopbarNavbarComponent
+    TopbarNavbarComponent,
+    E404Component,
+    BlankComponent,
+    ButtonsComponent,
+    CardsComponent,
+    ChartsComponent,
+    DashboardComponent,
+    ForgotPasswordComponent,
+    LoginComponent,
+    RegisterComponent,
+    TablesComponent,
+    ColorComponent,
+    BorderComponent,
+    AnimationComponent,
+    OthersComponent
   ],
   imports: [
     BrowserModule,
@@ -29,6 +57,7 @@ import { TopbarNavbarComponent } from './components/topbar-navbar/topbar-navbar.
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { 
 
   constructor() {
